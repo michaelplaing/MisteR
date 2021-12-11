e653429bb6044b8dfa74e64657c7d66eb5e094be
michaelplaing <mlaing@post.harvard.edu>
2021-12-11 13:17:05 +0000

    finish CONNECT table - works so far

--- test/pack.c
+++ test/pack.c
@@ -10,10 +10,13 @@ const uint8_t PNM[] = {0x00, 0x04, 'M', 'Q', 'T', 'T'};
 #define NA 0
 
 const connect_hdr CONNECT_HDRS_TEMPLATE[] = {
+//  Fixed Header
 //   name                   index   link        function            value           bitpos  vlen    exists  id      isalloc buflen  buf
     {"packet_type",         0,      "",         pack_uint8,         CMD_CONNECT,    NA,     1,      true,   NA,     false,  0,      NULL},
     {"remaining_length",    0,      "last",     pack_VBI,           0,              NA,     0,      true,   NA,     false,  0,      NULL},
-    {"protocol_name",       0,      "",         pack_char_buffer,   (Word_t)PNM,    NA,     PNMSZ,  true,   NA,     false,  0,      NULL},
+//  Variable Header
+//   name                   index   link        function            value           bitpos  vlen    exists  id      isalloc buflen  buf
+    {"protocol_name",       0,      "",         pack_char_buf,      (Word_t)PNM,    NA,     PNMSZ,  true,   NA,     false,  0,      NULL},
     {"protocol_version",    0,      "",         pack_uint8,         5,              NA,     2,      true,   NA,     false,  0,      NULL},
     {"reserved",            0,      "flags",    pack_in_parent,     0,              0,      1,      true,   NA,     false,  0,      NULL},
     {"clean_start",         0,      "flags",    pack_in_parent,     0,              1,      1,      true,   NA,     false,  0,      NULL},
@@ -22,11 +25,12 @@ const connect_hdr CONNECT_HDRS_TEMPLATE[] = {
     {"will_retain",         0,      "flags",    pack_in_parent,     0,              5,      1,      true,   NA,     false,  0,      NULL},
     {"password_flag",       0,      "flags",    pack_in_parent,     0,              6,      1,      true,   NA,     false,  0,      NULL},
     {"username_flag",       0,      "flags",    pack_in_parent,     0,              7,      1,      true,   NA,     false,  0,      NULL},
-    {"flags",               0,      "",         pack_flags_alloc,   NA,             NA,     NA,     true,   NA,     false,  0,      NULL},
-    {"keep_alive",          0,      "flags",    pack_uint16,        0,              NA,     2,      true,   NA,     false,  0,      NULL},
-    {"property_length",     0,      "receive_maximum",
-                                                pack_VBI,           0,              NA,     0,      true,   NA,     false,  0,      NULL},
+    {"flags",               0,      "",         pack_flags_alloc,   NA,             NA,     1,      true,   NA,     false,  0,      NULL},
+    {"keep_alive",          0,      "",         pack_uint16,        0,              NA,     2,      true,   NA,     false,  0,      NULL},
+//  Variable Header Properties
 //   name                   index   link        function            value           bitpos  vlen    exists  id      isalloc buflen  buf
+    {"property_length",     0,      "authentication_data",
+                                                pack_VBI,           0,              NA,     0,      true,   NA,     false,  0,      NULL},
     {"session_expiry",      0,      "",         pack_sprop_uint32,  0,              NA,     0,      false,  0x11,   false,  0,      NULL},
     {"receive_maximum",     0,      "",         pack_sprop_uint16,  0,              NA,     0,      false,  0x21,   false,  0,      NULL},
     {"maximum_packet_size", 0,      "",         pack_sprop_uint32,  0,              NA,     0,      false,  0x27,   false,  0,      NULL},
@@ -38,8 +42,27 @@ const connect_hdr CONNECT_HDRS_TEMPLATE[] = {
     {"user_properties",     0,      "",         pack_mprop_strpair, (Word_t)NULL,   NA,     0,      false,  0x26,   false,  0,      NULL},
     {"authentication_method",
                             0,      "",         pack_sprop_str,     (Word_t)NULL,   NA,     0,      false,  0x15,   false,  0,      NULL},
-    {"authentication_data", 0,      "",         pack_sprop_char_buf,(Word_t)NULL,   NA,     0,      false,  0x16,  false,  0,      NULL},
-    {"client_identifier",   0,      "",         pack_str,           (Word_t)NULL,   NA,     0,      false,  NA,     false,  0,      NULL}
+    {"authentication_data", 0,      "",         pack_sprop_char_buf,(Word_t)NULL,   NA,     0,      false,  0x16,   false,  0,      NULL},
+// Payload
+//   name                   index   link        function            value           bitpos  vlen    exists  id      isalloc buflen  buf
+    {"client_identifier",   0,      "",         pack_str,           (Word_t)NULL,   NA,     0,      false,  NA,     false,  0,      NULL},
+// Payload Will Properties
+    {"will_property_length",0,      "will_user_properties",
+                                                pack_VBI,           0,              NA,     0,      false,  NA,     false,  0,      NULL},
+    {"will_delay_interval", 0,      "",         pack_sprop_uint32,  0,              NA,     0,      false,  0x18,   false,  0,      NULL},
+    {"payload_format_indicator",
+                            0,      "",         pack_sprop_uint8,   0,              NA,     0,      false,  0x01,   false,  0,      NULL},
+    {"message_expiry_interval",
+                            0,      "",         pack_sprop_uint32,  0,              NA,     0,      false,  0x02,   false,  0,      NULL},
+    {"content_type",        0,      "",         pack_sprop_str,     (Word_t)NULL,   NA,     0,      false,  0x03,   false,  0,      NULL},
+    {"response_topic",      0,      "",         pack_sprop_str,     (Word_t)NULL,   NA,     0,      false,  0x08,   false,  0,      NULL},
+    {"correlation_data",    0,      "",         pack_sprop_char_buf,(Word_t)NULL,   NA,     0,      false,  0x09,   false,  0,      NULL},
+    {"will_user_properties",0,      "",         pack_mprop_strpair, (Word_t)NULL,   NA,     0,      false,  0x26,   false,  0,      NULL},
+// Payload (remainder)
+    {"will_topic",          0,      "",         pack_str,           (Word_t)NULL,   NA,     0,      false,  NA,     false,  0,      NULL},
+    {"will_payload",        0,      "",         pack_char_buf,      (Word_t)NULL,   NA,     0,      false,  NA,     false,  0,      NULL},
+    {"user_name",           0,      "",         pack_str,           (Word_t)NULL,   NA,     0,      false,  NA,     false,  0,      NULL},
+    {"password",            0,      "",         pack_char_buf,      (Word_t)NULL,   NA,     0,      false,  NA,     false,  0,      NULL}
 };
 
 int set_scalar_value(pack_ctx *pctx, char *name, Word_t value) {
@@ -64,6 +87,7 @@ int reset_header_value(pack_ctx *pctx, char *name) {
     JSLG(Pchdr, pctx->PJSLArray, name);
     (*Pchdr)->value = 0;
     (*Pchdr)->exists = false;
+    (*Pchdr)->vlen = 0;
     return 0;
 }
 
@@ -112,6 +136,8 @@ int make_VBI(uint32_t val32, uint8_t *buf) {
 
 //  calculate length, convert to VBI, & pack into buffer
 int pack_VBI(pack_ctx *pctx, connect_hdr *chdr) {
+    if (!chdr->exists) return 0;
+
     printf("pack_VBI: %s\n", chdr->name);
     size_t cum_len = 0;
     connect_hdr **Pchdr, *end_chdr, *current_chdr;
@@ -366,9 +392,12 @@ int pack_mprop_strpair(pack_ctx *pctx, connect_hdr *chdr) {
     return 0;
 }
 
-int pack_char_buffer(pack_ctx *pctx, connect_hdr *chdr) {
-    chdr->buflen = chdr->vlen;
-    chdr->buf = (uint8_t *)(chdr->value);
+int pack_char_buf(pack_ctx *pctx, connect_hdr *chdr) {
+    if (chdr->exists) {
+        chdr->buflen = chdr->vlen;
+        chdr->buf = (uint8_t *)chdr->value;
+    }
+
     return 0;
 }
 
