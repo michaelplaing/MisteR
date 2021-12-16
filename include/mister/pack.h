d7bca4fb6468034f99929602d27cadf738e77621
michaelplaing <mlaing@post.harvard.edu>
2021-12-15 20:26:57 -0500

    tweaks

--- include/mister/pack.h
+++ include/mister/pack.h
@@ -1,15 +1,9 @@
 #ifndef PACK_H
 #define PACK_H
 
-#ifndef _PVOID_T
-#define _PVOID_T
-typedef void *   Pvoid_t;
-typedef void ** PPvoid_t;
-#endif
-
 #ifndef _WORD_T
 #define _WORD_T
-typedef unsigned long    Word_t, * PWord_t;  // expect 32-bit or 64-bit words.
+typedef unsigned long Word_t;  // expect 32-bit or 64-bit words.
 #endif
 
 struct mr_mdata;
@@ -20,7 +14,6 @@ typedef struct pack_ctx {
     size_t len;
     struct mr_mdata *mdata0;
     size_t mdata_count;
-    Pvoid_t PJSLArray;
 } pack_ctx;
 
 typedef struct string_pair {
