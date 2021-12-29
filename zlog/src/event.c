6df8573683786929ed852f16c5730013f84cab19
michaelplaing <mlaing@post.harvard.edu>
2021-12-29 14:41:31 -0500

    fix format & int size

--- zlog/src/event.c
+++ zlog/src/event.c
@@ -29,7 +29,7 @@ void zlog_event_profile(zlog_event_t * a_event, int flag)
 			a_event->file, a_event->file_len,
 			a_event->func, a_event->func_len,
 			a_event->line, a_event->level,
-			a_event->hex_buf, a_event->str_format,	
+			a_event->hex_buf, a_event->str_format,
 			a_event->time_stamp.tv_sec, a_event->time_stamp.tv_usec,
 			(long)a_event->pid, (long)a_event->tid,
 			a_event->time_cache_count);
@@ -82,7 +82,7 @@ zlog_event_t *zlog_event_new(int time_cache_count)
 	a_event->tid = pthread_self();
 
 	a_event->tid_str_len = sprintf(a_event->tid_str, "%lu", (unsigned long)a_event->tid);
-	a_event->tid_hex_str_len = sprintf(a_event->tid_hex_str, "0x%x", (unsigned int)a_event->tid);
+	a_event->tid_hex_str_len = sprintf(a_event->tid_hex_str, "0x%lu", (unsigned long)a_event->tid);
 
 	//zlog_event_profile(a_event, ZC_DEBUG);
 	return a_event;
